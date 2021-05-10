b500eae9cf36ca59f3997cafca146632f63cadd4
Felix Galaup <felixgalaup@yahoo.fr>
2021-05-10 16:42:13 +0200

    [^]-Add free and remove empty line for exec.

--- Sources/_Common/Instruction/instruction.c
+++ Sources/_Common/Instruction/instruction.c
@@ -6,7 +6,7 @@
 /*   By: fgalaup <fgalaup@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/07 11:36:34 by fgalaup           #+#    #+#             */
-/*   Updated: 2021/05/10 13:49:03 by fgalaup          ###   ########lyon.fr   */
+/*   Updated: 2021/05/10 16:38:07 by fgalaup          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,14 @@ void	intruction_exec(t_common_context *context)
 	int		read_status;
 	int		i;
 
-	line = NULL;
 	read_status = FT_TRUE;
 	while (read_status)
 	{
 		i = 0;
+		ft_managed_free(line);
 		read_status = get_next_line(standard_input, &line);
+		if (line[0] == '\0')
+			continue ;
 		while (instruction_set[i].token != NULL)
 		{
 			if (ft_strncmp(line, instruction_set[i].token, 16) == 0)
@@ -36,4 +38,5 @@ void	intruction_exec(t_common_context *context)
 		if (instruction_set[i].token == NULL)
 			error_fatal(ERROR_INSTRUCTION_UNKNOWN, line);
 	}
+	ft_managed_free(line);
 }
